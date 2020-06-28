4cfd0e5726b4110c1dc8f3ff98f3d034479879bf
Davi <davi.goulart@elevaeducacao.com.br>
2020-06-28 17:54:04 -0300

    removing services

--- src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://us-central1-functions-bbfa2.cloudfunctions.net/api/",
-  timeout: 1000,
-  // headers: {'X-Custom-Header': 'foobar'}
-});
-
-export default api;
